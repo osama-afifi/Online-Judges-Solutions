f1f3ef3bfdcef8a28c27f700eca1f145b9d62d05
Osama M. Afifi <osama.egt@gmail.com>
2014-09-10 03:17:39 +0300

    Minimal Coverage - Greedy (Segment Covering)

--- UVA Problems/UVA/UVA/Fill.cpp
+++ UVA Problems/UVA/UVA/Fill.cpp
@@ -1,172 +1,172 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <cstdio>
-#include <queue>
-#include <vector>
-
-using namespace std;
-
-struct Node
-{
-	int a,b,c,cost;
-	Node()
-	{}
-	Node(int aa , int bb , int cc , int ccost) : a(aa), b(bb) , c(cc) , cost(ccost)
-	{}
-	bool operator <(const Node &a) const
-	{
-		return cost>a.cost;
-	}
-};
-
-int a,b,c,d;
-
-int ABS(int x)
-{
-if(x<0)return 0;
-else return x;
-}
-
-bool check(int x,int prev)
-{
-	if(x>=prev && x<=d)return true;
-	else return false;
-}
-
-int dist[201][201][201];
-bool vis[201][201][201];
-int const oo = 1<<27;
-
-int main()
-{
-	freopen("input.in","r",stdin);
-	int test,kase=0;
-	scanf("%d",&test);
-
-	int n1,n2,cost;
-	while(test--)
-	{
-		scanf("%d%d%d%d",&a,&b,&c,&d);
-		memset(vis,0,sizeof vis);
-
-		for(int i = 0 ; i<=a ; i++)
-			for(int j = 0 ; j<=b ; j++)
-				for(int k = 0 ; k<=c ; k++)
-					dist[i][j][k]=oo;
-
-		priority_queue <Node> DIJ;
-		Node cur = Node(0,0,c,0);
-		DIJ.push(cur);
-		bool found = false;
-		while(!DIJ.empty())
-		{
-			cur=DIJ.top();
-
-			//if(cur.a == d || cur.b == d || cur.c == d )		
-			//	break;
-
-			DIJ.pop();
-
-			//if(cur.a<0 || cur.b<0 || cur.c <0)continue;
-
-			if(cur.a && cur.b+cur.a<=b && cur.a<=b-cur.b && dist[0][cur.b+cur.a][cur.c]>cur.cost+cur.a)
-			{
-				DIJ.push(Node(0,cur.b+cur.a,cur.c,cur.cost+cur.a));
-				dist[0][cur.b+cur.a][cur.c]=cur.cost+cur.a;
-			}
-			if(cur.a && cur.c+cur.a<=c && cur.a<=c-cur.c && dist[0][cur.b][cur.c+cur.a]>cur.cost+cur.a)
-			{
-				DIJ.push(Node(0,cur.b,cur.c+cur.a,cur.cost+cur.a));
-				dist[0][cur.b][cur.c+cur.a]=cur.cost+cur.a;
-			}
-			if(cur.a && cur.a>=b-cur.b && dist[cur.a-(b-cur.b)][b][cur.c]>cur.cost+(b-cur.b))
-			{
-				DIJ.push(Node(cur.a-(b-cur.b),b,cur.c,cur.cost+(b-cur.b)));
-				dist[cur.a-(b-cur.b)][b][cur.c]=cur.cost+(b-cur.b);
-			}
-			if(cur.a &&  cur.a>=c-cur.c && dist[cur.a-(c-cur.c)][cur.b][c]>cur.cost+(c-cur.c) )
-			{
-				DIJ.push(Node(cur.a-(c-cur.c),cur.b,c,cur.cost+(c-cur.c)));
-				dist[cur.a-(c-cur.c)][cur.b][c]=cur.cost+(c-cur.c);
-			}
-
-			////
-
-			if(cur.b && cur.a+cur.b<=b && cur.b<=a-cur.a && dist[cur.a+cur.b][0][cur.c]>cur.cost+cur.b)
-			{
-				DIJ.push(Node(cur.a+cur.b,0,cur.c,cur.cost+cur.b));
-				dist[cur.a+cur.b][0][cur.c]=cur.cost+cur.b;
-			}
-			if(cur.b && cur.c+cur.b<=c  && cur.b<=c-cur.c && dist[cur.a][0][cur.c+cur.b]>cur.cost+cur.b)
-			{
-				DIJ.push(Node(cur.a,0,cur.c+cur.b,cur.cost+cur.b));
-				dist[cur.a][0][cur.c+cur.b]=cur.cost+cur.b;
-			}
-			if(cur.b && cur.b>=a-cur.a  && dist[a][cur.b-(a-cur.a)][cur.c]>cur.cost+(a-cur.a))
-			{
-				DIJ.push(Node(a,cur.b-(a-cur.a),cur.c,cur.cost+(a-cur.a)));
-				dist[a][cur.b-(a-cur.a)][cur.c]=cur.cost+(a-cur.a);
-			}
-			if(cur.b && cur.b>=c-cur.c && dist[cur.a][cur.b-(c-cur.c)][c]>cur.cost+(c-cur.c))
-			{
-				DIJ.push(Node(cur.a,cur.b-(c-cur.c),c,cur.cost+(c-cur.c)));
-				dist[cur.a][cur.b-(c-cur.c)][c]=cur.cost+(c-cur.c);
-			}
-
-			////
-
-			if(cur.c && cur.a+cur.c<=a && cur.c<=a-cur.a && dist[cur.a+cur.c][cur.b][0]>cur.cost+cur.c)
-			{
-				DIJ.push(Node(cur.a+cur.c,cur.b,0,cur.cost+cur.c));
-				dist[cur.a+cur.c][cur.b][0]=cur.cost+cur.c;
-			}
-			if(cur.c && cur.b+cur.c<=b &&  cur.c<=b-cur.b && dist[cur.a][cur.b+cur.c][0]>cur.cost+cur.c)
-			{
-				DIJ.push(Node(cur.a,cur.b+cur.c,0,cur.cost+cur.c));
-				dist[cur.a][cur.b+cur.c][0]=cur.cost+cur.c;
-			}
-			if(cur.c && cur.c>=a-cur.a  && dist[a][cur.b][cur.c-(a-cur.a)]>cur.cost+(a-cur.a))
-			{
-				DIJ.push(Node(a,cur.b,cur.c-(a-cur.a),cur.cost+(a-cur.a)));
-				dist[a][cur.b][cur.c-(a-cur.a)]=cur.cost+(a-cur.a);
-			}
-			if(cur.c && cur.c>=b-cur.b && dist[cur.a][b][cur.c-(b-cur.b)]>cur.cost+(b-cur.b))
-			{
-				DIJ.push(Node(cur.a,b,cur.c-(b-cur.b),cur.cost+(b-cur.b)));
-				dist[cur.a][b][cur.c-(b-cur.b)]=cur.cost+(b-cur.b);
-			}
-
-		}
-
-		int res=oo;
-		int prev=-1;
-		for(int i = 0 ; i<=a ; i++)
-			for(int j = 0 ; j<=b ; j++)
-				for(int k = 0 ; k<=c ; k++)
-				{
-					if(i<=d && (i==prev && dist[i][j][k]<res  || i>prev && dist[i][j][k]<oo  ))
-					{
-						res=dist[i][j][k];
-						prev=i;
-					}
-					if(j<=d && (j==prev && dist[i][j][k]<res  || j>prev && dist[i][j][k]<oo  ))
-					{
-						res=dist[i][j][k];
-						prev=j;
-					}
-				if(k<=d && (k==prev && dist[i][j][k]<res  || k>prev && dist[i][j][k]<oo  ))
-					{
-						res=dist[i][j][k];
-						prev=k;
-					}
-				}
-				if(abs(prev-d)<d)
-				cout<<res<<" "<<prev<<endl;
-				else
-					cout<<0<<" "<<prev<<endl;
-	}
-	return 0;
-
-
-}
+//#include <iostream>
+//#include <cstring>
+//#include <algorithm>
+//#include <cstdio>
+//#include <queue>
+//#include <vector>
+//
+//using namespace std;
+//
+//struct Node
+//{
+//	int a,b,c,cost;
+//	Node()
+//	{}
+//	Node(int aa , int bb , int cc , int ccost) : a(aa), b(bb) , c(cc) , cost(ccost)
+//	{}
+//	bool operator <(const Node &a) const
+//	{
+//		return cost>a.cost;
+//	}
+//};
+//
+//int a,b,c,d;
+//
+//int ABS(int x)
+//{
+//if(x<0)return 0;
+//else return x;
+//}
+//
+//bool check(int x,int prev)
+//{
+//	if(x>=prev && x<=d)return true;
+//	else return false;
+//}
+//
+//int dist[201][201][201];
+//bool vis[201][201][201];
+//int const oo = 1<<27;
+//
+//int main()
+//{
+//	freopen("input.in","r",stdin);
+//	int test,kase=0;
+//	scanf("%d",&test);
+//
+//	int n1,n2,cost;
+//	while(test--)
+//	{
+//		scanf("%d%d%d%d",&a,&b,&c,&d);
+//		memset(vis,0,sizeof vis);
+//
+//		for(int i = 0 ; i<=a ; i++)
+//			for(int j = 0 ; j<=b ; j++)
+//				for(int k = 0 ; k<=c ; k++)
+//					dist[i][j][k]=oo;
+//
+//		priority_queue <Node> DIJ;
+//		Node cur = Node(0,0,c,0);
+//		DIJ.push(cur);
+//		bool found = false;
+//		while(!DIJ.empty())
+//		{
+//			cur=DIJ.top();
+//
+//			//if(cur.a == d || cur.b == d || cur.c == d )		
+//			//	break;
+//
+//			DIJ.pop();
+//
+//			//if(cur.a<0 || cur.b<0 || cur.c <0)continue;
+//
+//			if(cur.a && cur.b+cur.a<=b && cur.a<=b-cur.b && dist[0][cur.b+cur.a][cur.c]>cur.cost+cur.a)
+//			{
+//				DIJ.push(Node(0,cur.b+cur.a,cur.c,cur.cost+cur.a));
+//				dist[0][cur.b+cur.a][cur.c]=cur.cost+cur.a;
+//			}
+//			if(cur.a && cur.c+cur.a<=c && cur.a<=c-cur.c && dist[0][cur.b][cur.c+cur.a]>cur.cost+cur.a)
+//			{
+//				DIJ.push(Node(0,cur.b,cur.c+cur.a,cur.cost+cur.a));
+//				dist[0][cur.b][cur.c+cur.a]=cur.cost+cur.a;
+//			}
+//			if(cur.a && cur.a>=b-cur.b && dist[cur.a-(b-cur.b)][b][cur.c]>cur.cost+(b-cur.b))
+//			{
+//				DIJ.push(Node(cur.a-(b-cur.b),b,cur.c,cur.cost+(b-cur.b)));
+//				dist[cur.a-(b-cur.b)][b][cur.c]=cur.cost+(b-cur.b);
+//			}
+//			if(cur.a &&  cur.a>=c-cur.c && dist[cur.a-(c-cur.c)][cur.b][c]>cur.cost+(c-cur.c) )
+//			{
+//				DIJ.push(Node(cur.a-(c-cur.c),cur.b,c,cur.cost+(c-cur.c)));
+//				dist[cur.a-(c-cur.c)][cur.b][c]=cur.cost+(c-cur.c);
+//			}
+//
+//			////
+//
+//			if(cur.b && cur.a+cur.b<=b && cur.b<=a-cur.a && dist[cur.a+cur.b][0][cur.c]>cur.cost+cur.b)
+//			{
+//				DIJ.push(Node(cur.a+cur.b,0,cur.c,cur.cost+cur.b));
+//				dist[cur.a+cur.b][0][cur.c]=cur.cost+cur.b;
+//			}
+//			if(cur.b && cur.c+cur.b<=c  && cur.b<=c-cur.c && dist[cur.a][0][cur.c+cur.b]>cur.cost+cur.b)
+//			{
+//				DIJ.push(Node(cur.a,0,cur.c+cur.b,cur.cost+cur.b));
+//				dist[cur.a][0][cur.c+cur.b]=cur.cost+cur.b;
+//			}
+//			if(cur.b && cur.b>=a-cur.a  && dist[a][cur.b-(a-cur.a)][cur.c]>cur.cost+(a-cur.a))
+//			{
+//				DIJ.push(Node(a,cur.b-(a-cur.a),cur.c,cur.cost+(a-cur.a)));
+//				dist[a][cur.b-(a-cur.a)][cur.c]=cur.cost+(a-cur.a);
+//			}
+//			if(cur.b && cur.b>=c-cur.c && dist[cur.a][cur.b-(c-cur.c)][c]>cur.cost+(c-cur.c))
+//			{
+//				DIJ.push(Node(cur.a,cur.b-(c-cur.c),c,cur.cost+(c-cur.c)));
+//				dist[cur.a][cur.b-(c-cur.c)][c]=cur.cost+(c-cur.c);
+//			}
+//
+//			////
+//
+//			if(cur.c && cur.a+cur.c<=a && cur.c<=a-cur.a && dist[cur.a+cur.c][cur.b][0]>cur.cost+cur.c)
+//			{
+//				DIJ.push(Node(cur.a+cur.c,cur.b,0,cur.cost+cur.c));
+//				dist[cur.a+cur.c][cur.b][0]=cur.cost+cur.c;
+//			}
+//			if(cur.c && cur.b+cur.c<=b &&  cur.c<=b-cur.b && dist[cur.a][cur.b+cur.c][0]>cur.cost+cur.c)
+//			{
+//				DIJ.push(Node(cur.a,cur.b+cur.c,0,cur.cost+cur.c));
+//				dist[cur.a][cur.b+cur.c][0]=cur.cost+cur.c;
+//			}
+//			if(cur.c && cur.c>=a-cur.a  && dist[a][cur.b][cur.c-(a-cur.a)]>cur.cost+(a-cur.a))
+//			{
+//				DIJ.push(Node(a,cur.b,cur.c-(a-cur.a),cur.cost+(a-cur.a)));
+//				dist[a][cur.b][cur.c-(a-cur.a)]=cur.cost+(a-cur.a);
+//			}
+//			if(cur.c && cur.c>=b-cur.b && dist[cur.a][b][cur.c-(b-cur.b)]>cur.cost+(b-cur.b))
+//			{
+//				DIJ.push(Node(cur.a,b,cur.c-(b-cur.b),cur.cost+(b-cur.b)));
+//				dist[cur.a][b][cur.c-(b-cur.b)]=cur.cost+(b-cur.b);
+//			}
+//
+//		}
+//
+//		int res=oo;
+//		int prev=-1;
+//		for(int i = 0 ; i<=a ; i++)
+//			for(int j = 0 ; j<=b ; j++)
+//				for(int k = 0 ; k<=c ; k++)
+//				{
+//					if(i<=d && (i==prev && dist[i][j][k]<res  || i>prev && dist[i][j][k]<oo  ))
+//					{
+//						res=dist[i][j][k];
+//						prev=i;
+//					}
+//					if(j<=d && (j==prev && dist[i][j][k]<res  || j>prev && dist[i][j][k]<oo  ))
+//					{
+//						res=dist[i][j][k];
+//						prev=j;
+//					}
+//				if(k<=d && (k==prev && dist[i][j][k]<res  || k>prev && dist[i][j][k]<oo  ))
+//					{
+//						res=dist[i][j][k];
+//						prev=k;
+//					}
+//				}
+//				if(abs(prev-d)<d)
+//				cout<<res<<" "<<prev<<endl;
+//				else
+//					cout<<0<<" "<<prev<<endl;
+//	}
+//	return 0;
+//
+//
+//}
--- UVA Problems/UVA/UVA/Minimal coverage.cpp
+++ UVA Problems/UVA/UVA/Minimal coverage.cpp
@@ -1,68 +1,91 @@
-#include<iostream>
-#include<iomanip>
-#include<string>
-#include<cstring>
-#include<stdio.h>
-#include<algorithm>
-#include<vector>
-#include<map>
-#include<sstream>
-#include<queue>
-#include<set>
-#include<stack>
-#include<math.h>
-
-
-#define FOR(i, a, b) for( int i = (a); i < (b); i++ )
-#define ALL(A) A.begin(), A.end()
-#define Set(a, s) memset(a, s, sizeof (a))
-#define pb push_back
-#define mp make_pair
-typedef long long LL;
-using namespace std;
-
-int n;
-int m;
-int dp[5509];
-vector<pair<int,int>  >arr;
-
-inline int bs(int idx)
-{
-	int b=
-}
-
-int solve(int i)
-{
-	if(i>=m)
-		return 0;
-	if(dp[i]!=-1)
-		return dp[i];
-	int mini = 1<<25;
-	
-
-
-	dp[i] = mini;
-}
-
-int main()
-{
-	freopen("input.in", "r" , stdin);
-
-	while(cin>>m)
-	{
-		while(1)
-		{
-			pair<int,int>p;
-			cin>>p.first>>p.second;
-			if(p==mp(0,0))break;
-			arr.push_back(p);
-		}
-
-		sort(arr.begin(),arr.end());
-		Set(dp,-1);
-		int res = solve(0);
-		cout << res <<endl;
-	}
-
-	return 0;
-}
+//#include<iostream>
+//#include<iomanip>
+//#include<string>
+//#include<cstring>
+//#include<stdio.h>
+//#include<algorithm>
+//#include<vector>
+//#include<map>
+//#include<sstream>
+//#include<queue>
+//#include<set>
+//#include<stack>
+//#include<math.h>
+//
+//
+//#define FOR(i, a, b) for( int i = (a); i < (b); i++ )
+//#define ALL(A) A.begin(), A.end()
+//#define Set(a, s) memset(a, s, sizeof (a))
+//#define pb push_back
+//#define mp make_pair
+//typedef long long LL;
+//using namespace std;
+//
+//int m;
+//vector<pair<int,int>  >arr;
+//
+//int main()
+//{
+//	freopen("input.in", "r" , stdin);
+//
+//	int t;
+//	cin>>t;
+//	while(t--)
+//	{
+//		arr.clear();
+//		cin>>m;
+//		while(1)
+//		{
+//			pair<int,int>p;
+//			cin>>p.first>>p.second;
+//			if(p==mp(0,0))break;
+//			arr.push_back(p);
+//		}
+//
+//		sort(arr.begin(),arr.end());
+//		vector<int>ans;
+//
+//		int a=0,b=0;
+//		int i=0;
+//		while(b<m)
+//		{
+//			// find max covering segment
+//			int idx=-1,best=0;
+//			b=a;
+//			while(i<arr.size() && arr[i].first<=a)
+//			{
+//				if(arr[i].second>=b)
+//				{
+//					idx = i;
+//					b = arr[i].second;
+//				}
+//				++i;
+//			}
+//
+//			if(idx==-1)
+//				break;
+//			else
+//			{
+//				ans.push_back(idx);
+//				a = b = arr[idx].second;
+//			}
+//
+//		}
+//
+//		if(b>=m)
+//		{
+//			cout << ans.size() << endl;
+//			FOR(i,0,ans.size())
+//				cout << arr[ans[i]].first << " " << arr[ans[i]].second <<endl;
+//
+//		}
+//		else
+//		{
+//			cout << 0 <<endl;
+//		}
+//		if(t)
+//			cout <<endl;
+//	}
+//
+//	return 0;
+//}
